98a0be32a8491f7e660a5a44787f611b07e56730
Adam "Bebo" Kimble <Bebo.Kimble@gmail.com>
2014-10-04 15:49:31 -0400

    working replacement

--- js/Main.js
+++ js/Main.js
@@ -1,33 +1,53 @@
 var conversion_list = [
-	["inch","m",0.0254],
-	["inches","m",0.0254],
-	["in","m",0.0254],
-	["foot","m",0.3048],
-	["feet","m",0.3048],
-	["ft","m",0.3048],
-	["yard","m",0.9144],
-	["yd","m",0.9144],
-	["mile","m",1609.34],
-	["mi","m",1609.34],
-	["teaspoon","l",0.004929],
-	["tsp","l",0.004929],
-	["tablespoon","l",0.01479],
-	["tbsp","l",0.01479],
-	["fluid ounce","l",0.02957],
-	["fl oz","l",0.02957],
-	["cup","l",0.2366],
-	["pint","l",0.4732],
-	["quart","l",0.9464],
-	["gallon","l",3.785],
-	["ounce","g",28.35],
-	["oz","g",28.35],
-	["pound","g",453.6],
-	["lb","g",453.6],
-	["ton","g",907185],
-	["fahrenheit","°c",""],
+	[" inch","m",0.0254],
+	[" inches","m",0.0254],
+	[" in.","m",0.0254],
+	[" foot","m",0.3048],
+	[" feet","m",0.3048],
+	[" ft","m",0.3048],
+	[" yard","m",0.9144],
+	[" yd","m",0.9144],
+	[" mile","m",1609.34],
+	[" mi.","m",1609.34],
+	[" teaspoon","l",0.004929],
+	[" tsp","l",0.004929],
+	[" tablespoon","l",0.01479],
+	[" tbsp","l",0.01479],
+	[" fluid ounce","l",0.02957],
+	[" fl oz","l",0.02957],
+	[" cup","l",0.2366],
+	[" pint","l",0.4732],
+	[" quart","l",0.9464],
+	[" gallon","l",3.785],
+	[" ounce","g",28.35],
+	[" oz","g",28.35],
+	[" pound","g",453.6],
+	[" lb","g",453.6],
+	[" ton","g",907185],
+	[" fahrenheit","°c",""],
 	["°f","°c",""],
-	["mph","kph",1.609]
+	[" mph","kph",1.609]
 	];
+ 
+ function grab_number (text, start) {
+	var add = /[0-9]|[.]|,|\s/;
+	var value = "";
+	text = text.split('');
+	console.log("105");
+	while (start > 0) {
+		start--;
+	console.log(text[start].match(add) != null);
+		if(text[start].match(add) != null) {
+			value = text[start] + value;
+		} else  {
+			start = -1; //exit loop
+		}
+	}
+	if(value.length = 0){value = "0";}
+	console.log("grab_number value: "+ value);
+	return value;
+}
+
  
 function replace_unit(s) {
   //TODO format string
@@ -106,44 +126,44 @@ for(i=0; i<s.length; i++) {
 return "<hover original='" + number + " " + unit + "' onmouseover='AddOriginalMeasurement(this)' onmouseout='RemoveOriginalMeasurement(this)'>" + m_number + " " + m_unit + "</hover>";
 }
 
-/////////////////////////////////////////////Steve's convert
-function convert (text) {
-	if (text != "") { 
-		text = text.toLowerCase();
-		var imperial = grab_imperial(text);
-		if (imperial != -1) {
-
-			var metric = replace_unit(imperial);
-			var impIndex  = text.search(imperial);
-			var front = text.substring(0, impIndex);
-			var back = text.substring(impIndex+imperial.length+1,text.length);
-		
-			front = convert(front);
-			back = convert(back);
-			text = front + metric + back;
-			
-			console.log("***** UNIT REPlACED ***** \"" + imperial + "\" \"" + metric + "\"");
-			imperial = grab_imperial(text);
-		}
-	} 
-	console.log("***** FINISHED CONVERTING *****");
-	return text;
+function replaceUnitBebo(s){
+var num,unitID,temp;
+unitID =0;
+console.log("RU 1");
+num = grab_number(s,0);
+console.log("RU 2");
+var convert = parseFloat(num);
+console.log("RU 3");
+for (var i = 0;i < conversion_list.length;i++) {
+		temp =s.search(conversion_list[i][0]);
+		if(temp > 0){unitID = i;}
+}
+console.log("RU 4 : " + unitID);
+convert *= conversion_list[unitID][2];
+console.log("RU DONE");
+return ""+convert+conversion_list[unitID][1];
 }
 
-function grab_imperial (text) {
-	var place_unit = next_unit_index(text);
-	var number = -1;
-	var unit = "";
-	if (place_unit[0] != -1 ) {
-		number = grab_number(text, place_unit[0]);
-		unit = conversion_list[place_unit[1]][0];
-	}
-	console.log("grab_imperial number+unit: " + number + unit);
-	return "" + number + unit;
+/////////////////////////////////////////////Steve's convert
+function convert (text) {
+var nextUnit = next_unit_index(text);
+while (nextUnit[0] > 0) {
+	console.log("Got unit at index " + nextUnit[0]);
+		var imperialStr = grab_number(text,nextUnit[0])+conversion_list[nextUnit[1]][0];
+		console.log("Got total imperial value "+ imperialStr);
+		var metric = replaceUnitBebo(imperialStr);
+		console.log("Unit replaced with metric val " + metric);
+		var front = text.substring(0, nextUnit[0]);
+		var back = text.substring(nextUnit[0]+imperialStr.length+1,text.length);
+		text = front + metric + back;
+		console.log("***** UNIT REPlACED ***** \"" + imperialStr + "\" \"" + metric + "\"");
+		nextUnit = next_unit_index(text);
+}
+console.log("***** FINISHED CONVERTING *****");
+return text;
 }
 
 function next_unit_index (text) {
-	text = text.toLowerCase();
 	var place_unit = new Array(2); // [0] = location of unit in text; [1] = unit that was found
 	place_unit[0] = -1;
 	place_unit[1] = 0;
@@ -151,31 +171,12 @@ function next_unit_index (text) {
 	while (place_unit[0] == -1 && place_unit[1] < conversion_list.length) {
 		place_unit[0] = text.search(conversion_list[place_unit[1]][0]);//
 		place_unit[1]++;
-		
 	}
 	place_unit[1]--;
-	console.log(place_unit[1] + " " + place_unit[0])
-	console.log("next_unit_index place_unit: " + place_unit);
+	console.log(place_unit[1] + " " + place_unit[0]);
 	return place_unit;
 }
 
-function grab_number (text, start) {
-	var add = /[0-9]|[.]|,|\s/;
-	var value = "";
-	text = text.split('');
-	console.log("105");
-	while (start > 0) {
-		start--;
-	console.log(text[start].match(add) != null);
-		if(text[start].match(add) != null) {
-			value = text[start] + value;
-		} else  {
-			start = -1; //exit loop
-		}
-	}
-	console.log("grab_number value: "+ value);
-	return value;
-}
 
 
 //Run main html parser
