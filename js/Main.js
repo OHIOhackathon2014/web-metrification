f3a3417dcd0a90a507a72ad3d73bc5f371db1fb6
Adam "Bebo" Kimble <Bebo.Kimble@gmail.com>
2014-10-04 02:33:08 -0400

    Things work

--- js/Main.js
+++ js/Main.js
@@ -1,7 +1,29 @@
+//Run main html parser
 
+try{
+var array = document.body.getElementsByTagName("*");
+var element;
+var length = array.length;
+for(var i=0; i<length;i++){
+element = array[i];
+var node =  element.childNodes[0];
+while(node != null){
+if(node.nodeType == 3){node.textContent = convert(node.textContent);}
+node = node.nextSibling;
+}
+}
 
+}
+catch(err){
 var oNewP = document.createElement("p");
-var oText = document.createTextNode("OHHHHHHHH YEAAAAAAAAAAAAAAAAAAHHHHHHHHH!!!!!!!!!!");
+var oText = document.createTextNode(err.message);
 oNewP.appendChild(oText);
 oNewP.style.fontSize = "xx-large";
 document.body.appendChild(oNewP);
+}
+
+var NewP = document.createElement("p");
+var Text = document.createTextNode("Done!");
+NewP.appendChild(Text);
+NewP.style.fontSize = "xx-large";
+document.body.appendChild(NewP);
