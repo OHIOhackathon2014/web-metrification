df335321be615a2778b7010975faa98ff5e76ca2
Adam "Bebo" Kimble <Bebo.Kimble@gmail.com>
2014-10-03 22:38:48 -0400

    Added a file
    Actually really added a real file somehow

---
+++ Main.js
@@ -0,0 +1,26 @@
+
+function parsePage() { 
+
+int paragraphCount = document.getElementsByTagName('p);
+for(int i=0;i<paragraphCount;i++){
+		
+
+}
+
+
+
+}
+
+function parseText(){
+var updatedText = "";
+
+
+
+
+
+
+return updatedText;
+}
+
+
+document.getElementsByTagName();
