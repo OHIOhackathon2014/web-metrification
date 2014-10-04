aa22fe1494de5280cbb845f5804e3461eaa89215
xwunderlich <xanderwunderlich.gmail.com>
2014-10-04 17:29:06 -0400

    added adjust to main

--- js/Main.js
+++ js/Main.js
@@ -45,6 +45,29 @@ var conversion_list = [
 	return value;
 }
 
+function adjust_unit(m_number, m_unit) {
+var m_unit_shift = 0;
+var offsets["","k","M","G","T","P","E","Z","Y"];
+var neg_offsets["","m","µ","n","p","f","a","z","y"];
+var m_adj_unit;
+
+while(m_number>1000 && m_unit_shift<8) {
+m_number/=1000;
+m_unit_shift++;
+}
+while(m_number<0 && m_unit_shift>-8) {
+m_number*=1000;
+m_unit_shift--;
+}
+
+if(m_unit_shift>0) {
+m_adj_unit = offsets[m_unit_shift] + m_unit;
+} else {
+m_adj_unit = neg_offsets[m_unit_shift] + m_unit;
+}
+
+return m_number+m_adj_unit;
+}
 function replaceUnit(s){
 var num,unitID,temp;
 unitID =0;
@@ -60,7 +83,7 @@ for (var i = 0;i < conversion_list.length;i++) {
 console.log("RU 4 : " + unitID);
 convert *= conversion_list[unitID][2];
 console.log("RU DONE");
-return " "+convert+conversion_list[unitID][1]+" ";
+return " "+adjust_unit(convert+conversion_list[unitID][1])+" ";
 }
 
 /////////////////////////////////////////////Steve's convert
