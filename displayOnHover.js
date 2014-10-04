e9f18153ef3e88ecb1c40b0b23e60379667b950f
BAM <moore.3071@osu.edu>
2014-10-04 06:04:24 -0400

    more fixes to push about hover

--- displayOnHover.js
+++ displayOnHover.js
@@ -2,6 +2,11 @@ function AddOriginalMeasurement(x) {
     var text = document.createElement("TEXTAREA");
     text.id = "673ValueDIsplayerOfThePluginInExtension";
     text.innerHTML = "Original value: "+x.original;
+    var event = event || window.event;
+    text.style.position = "absolute";
+    text.style.left = event.clientX;
+    text.style.top = event.clientY;
+    text.style.opacity = "0.5";
 }
 
 function RemoveOriginalMeasurement(x) {
